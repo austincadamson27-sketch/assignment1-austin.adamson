bef645e00c7e009c11b5a0ccee8417394acc8477
austincadamson27-sketch <austincadamson27@gmail.com>
2026-04-19 21:07:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const themeButton = document.getElementById("theme-toggle");
+document.addEventListener("DOMContentLoaded", function () {
+  const themeButton = document.getElementById("theme-toggle");
 
-themeButton.addEventListener("click", function () {
-  document.body.classList.toggle("dark-mode");
+  themeButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
 
-  if (document.body.classList.contains("dark-mode")) {
-    themeButton.textContent = "Switch to Light Mode";
-  } else {
-    themeButton.textContent = "Switch to Dark Mode";
-  }
+    if (document.body.classList.contains("dark-mode")) {
+      themeButton.textContent = "Switch to Light Mode";
+    } else {
+      themeButton.textContent = "Switch to Dark Mode";
+    }
+  });
 });
